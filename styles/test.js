edd47c2fcb9f139472967c1b1a8237e843b61efa
Orenier <usgnus425@naver.com>
2024-12-21 07:47:43 +0900

    test

--- styles/test.js
+++ styles/test.js
@@ -6,4 +6,4 @@ async function importPage(target) {
     document.querySelector('#' + target).innerHTML = await fetchHtmlAsText(target + '.html');
 }
 
-importPage('./변시널 판타지아/contents');
+importPage('./Hensinal_Fantasia/contents');
