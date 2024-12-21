477dc8750dfb745f8a497598c7c33a2c3e49e356
Orenier <usgnus425@naver.com>
2024-12-21 11:15:53 +0900

    test

--- styles/test.js
+++
@@ -1,9 +0,0 @@
-async function fetchHtmlAsText(url) {
-    return await (await fetch(url)).text();
-}
-
-async function importPage(target) {
-    document.querySelector('#' + target).innerHTML = await fetchHtmlAsText('./Hensinal_Fantasia/' + target + '.html');
-}
-
-importPage('contents');
